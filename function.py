830f658c5bf014d5a5cf04397bf73c541e7ba167
gus-preto <0000119018453sp@al.educacao.sp.gov.br>
2025-04-29 12:14:06 -0300

    sistema modificado com a criação de um dicionario utilizando os (bigodes) sendo o nome e a idade cadastrada

--- function.py
+++ function.py
@@ -1,16 +1,26 @@
-def soma():
-    nun_01 = 18
-    nun_02 = 30
+# def soma():
+#     nun_01 = 18
+#     nun_02 = 30
 
-    resultado = nun_01 + nun_02
+#     resultado = nun_01 + nun_02
 
-    return resultado
+#     return resultado
 
-def dividirDoisNumeros(nota_01, nota_02):
-    resultado = nota_01 / nota_02
+# def dividirDoisNumeros(nota_01, nota_02):
+#     resultado = nota_01 / nota_02
 
-    return print(resultado)
+#     return print(resultado)
 
-dividirDoisNumeros(10, 2)
+# dividirDoisNumeros(10, 2)
 # multiplicarDoisNumeros(2, 10)
 # somarDoisNumeros(30, 90)
+
+def cadastrar(nome, idade):
+    data = {
+        "nome": nome, 
+        "idade": idade
+    }
+
+    return print(data)
+
+cadastrar("gustavo", "16")
