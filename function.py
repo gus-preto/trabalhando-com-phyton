918d602eb16e55fb84f554dd651b971a2aac5cb6
gus-preto <0000119018453sp@al.educacao.sp.gov.br>
2025-04-29 10:20:29 -0300

    sistema modificdo com a criação de tres sistemas de soma, multiplicação e divisao

---
+++ function.py
@@ -0,0 +1,16 @@
+def soma():
+    nun_01 = 18
+    nun_02 = 30
+
+    resultado = nun_01 + nun_02
+
+    return resultado
+
+def dividirDoisNumeros(nota_01, nota_02):
+    resultado = nota_01 / nota_02
+
+    return print(resultado)
+
+dividirDoisNumeros(10, 2)
+# multiplicarDoisNumeros(2, 10)
+# somarDoisNumeros(30, 90)
